dd0737313bb1a321c60c8bf049877dfb81ec8d03
Gabriel Pinheiro <gabriel.pinheiro.dev@outlook.com>
2022-11-21 16:04:47 -0300

    alterações nas rotas

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile(__dirname + "/public/pages/index.html");
 })
 app.get("/adote", (req, res) => {
   res.send("Hello World!");
@@ -15,5 +15,8 @@ app.get("/pet/:nome", (req, res) => {
   res.send("Hello World!");
 })
 app.get("/adote/:nome", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile(__dirname + `/public/pages/adote${req.params.nome}.html`);
+  
 })
+app.use(express.static(__dirname + '/public'));
+app.listen(3000, () => {console.log('aberto')})
